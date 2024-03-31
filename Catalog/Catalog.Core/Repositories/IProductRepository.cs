3d0f1d41cc9eff73e0784761f5e8f2863344219a
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2024-03-31 09:34:41 +0330

    product entity and repo

---
+++ Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<Product> GetProductByName(string name);
+        Task<Product> GetProductByBrand(string name);
+        Task<Product> CreatePoduct(Product name);
+        Task<bool> UpdatePoduct(Product name);
+        Task<bool> DeletePoduct(string id);
+
+
+    }
+}
