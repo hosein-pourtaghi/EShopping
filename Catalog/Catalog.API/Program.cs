c603373cba3a2c3792a6c174ecd738d3378f8740
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2024-03-31 09:06:12 +0330

    launch setting

--- Catalog/Catalog.API/Program.cs
+++ Catalog/Catalog.API/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
  
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseRouting();
 app.UseStaticFiles();
